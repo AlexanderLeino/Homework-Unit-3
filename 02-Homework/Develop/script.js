3b8e6b40bd306fa2dac55b8e34e59ae2d2631265
Alexander Leino <Alexander.Leino@yahoo.com>
2021-08-09 23:38:07 -0400

    Just need to fix scoping issue.

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -10,53 +10,55 @@ Would you like your password to have lowerCaseLetters?
 Would you like your */
 
 // Assignment Code
+var newArray = []
+var randomNumber
 var numberOfCharacters
-var password
+var choiceLowerCaseLetters
+var choiceSpecialCharacters
+var choiceUpperCaseLetters
+var choiceNumbers
+var generateBtn =  document.querySelector("#generate");
 var upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var lowerCaseLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var numbers = ['0','1,','2','3', '4', '5', '6', '7', '8', '9']  
 var specialCharacters = ['!', '#', '$', '%', '&', '(', ')', ,'*', '+', ',' ,'-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '{', '}','~']
 
 
-// Write password to the #password input
-function writePassword() {var numberOfCharacters = prompt ('How many characters would you like your password to be? MUST BE A NUMBER!!!')
-console.log(numberOfCharacters)
-//if (isNaN(numberOfCharacters)){console.log(alert('This is not a Valid Choice'))}
-//else {
-//  console.log("This is a valid choice");
-//}
-//}
-var choiceUpperCaseLetters = confirm('Would you like your password to contain uppercase letters?')
-if (choiceUpperCaseLetters === true){
-  console.log('they want uppercase letters')}
-  else {
-    console.log('They dont want their password to container uppercase letters')
-  }
-
-var choiceLowerCaseLetters = confirm('Would you like your password to container lowercase letters?')
-if (choiceLowerCaseLetters = true) {
-  console.log('They want their password to container lowercase letters')}
-  else {
-    console.log('They dont want lowercase numbers with their password.')
-  }
-
-  var choiceSpecialCharacters = confirm('Would you like your password to container special characters?')
-  if (choiceSpecialCharacters = true) {
-    console.log('They want special characters their password')}
-    else {
-      console.log('They dont want special characters with their password.')
-    
-
-var generateBtn = document.querySelector("#generate"); //The Document method querySelector() returns the first Element within the document that matches the specified selector, or group of selectors. If no matches are found, null is returned. 
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password; //The value property sets or returns the value of the value attribute of a text field
-
+function generatePassword() {
+for (i = 0; i < numberOfCharacters; i++){
+randomNumber = Math.floor(Math.random()* numberOfCharacters)
+console.log(randomNumber)
+newArray[randomNumber]}
 }
 
-// Add event listener to generate button
+
+// Write password to the #password input
+function writePassword() {
+  numberOfCharacters = prompt ('Pick a number between 8-128 and that will represent how the password you will generate will be.')
+  choiceUpperCaseLetters = confirm('Would you like your password to contain uppercase letters?')
+  choiceLowerCaseLetters = confirm('Would you like your password to contain lowercase letters?')
+  choiceSpecialCharacters = confirm('Would you like your password to contain special characters?')
+  choiceNumbers = confirm('Would you like your password to containe numbers?')
+  
+  if(choiceUpperCaseLetters) {
+    newArray.push(upperCaseLetters)
+  }
+  if(choiceLowerCaseLetters){
+  {newArray.push(lowerCaseLetters)
+  }
+  
+  if(choiceNumbers) {
+    newArray.push(numbers)
+  }
+  if(choiceSpecialCharacters){
+    newArray.push(specialCharacters)
+  }
+  const reducer = (accumulator, currentValue) => accumulator + currentValue; // this takes the independ objects in the array and combine them into one object
+  console.log(newArray.reduce(reducer))
+}
+var password = generatePassword()
+var passwordText = document.querySelector("#password");
+passwordText.value = password;
+}
 
 generateBtn.addEventListener("click", writePassword());
-}
